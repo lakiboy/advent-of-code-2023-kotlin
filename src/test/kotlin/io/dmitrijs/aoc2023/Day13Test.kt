d47d3d9e251f641ac034d23b87501bfb82d793c9
Dmitrijs Lakachauskis <d@dmitrijs.io>
2023-12-13 18:45:21 +0200

    Day 13 (#14)

---
+++ src/test/kotlin/io/dmitrijs/aoc2023/Day13Test.kt
@@ -0,0 +1,41 @@
+package io.dmitrijs.aoc2023
+
+import io.dmitrijs.aoc2023.Resources.resourceAsText
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+@DisplayName("Day 13")
+internal class Day13Test {
+    private val exampleInput = resourceAsText("day13_example")
+    private val problemInput = resourceAsText("day13")
+
+    @Nested
+    @DisplayName("Puzzle 1")
+    inner class Puzzle1 {
+        @Test
+        fun `solves example`() {
+            assertEquals(405, Day13(exampleInput).puzzle1())
+        }
+
+        @Test
+        fun `solves problem`() {
+            assertEquals(29_130, Day13(problemInput).puzzle1())
+        }
+    }
+
+    @Nested
+    @DisplayName("Puzzle 2")
+    inner class Puzzle2 {
+        @Test
+        fun `solves example`() {
+            assertEquals(400, Day13(exampleInput).puzzle2())
+        }
+
+        @Test
+        fun `solves problem`() {
+            assertEquals(33_438, Day13(problemInput).puzzle2())
+        }
+    }
+}
