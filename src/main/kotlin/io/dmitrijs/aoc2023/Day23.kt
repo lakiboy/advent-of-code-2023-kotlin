ba764923c4dc05679483ed1cb5f8657fe6eff840
Dmitrijs Lakachauskis <d@dmitrijs.io>
2023-12-23 13:56:13 +0200

    Day 23 - Part II (#29)

--- src/main/kotlin/io/dmitrijs/aoc2023/Day23.kt
+++ src/main/kotlin/io/dmitrijs/aoc2023/Day23.kt
@@ -4,6 +4,7 @@ import io.dmitrijs.aoc2023.Direction.DOWN
 import io.dmitrijs.aoc2023.Direction.LEFT
 import io.dmitrijs.aoc2023.Direction.RIGHT
 import io.dmitrijs.aoc2023.Direction.UP
+import kotlin.math.max
 
 class Day23(private val input: List<String>) {
     private val maxY = input.size
@@ -12,14 +13,14 @@ class Day23(private val input: List<String>) {
     private val finish = Point(maxX - 2, maxY - 1)
 
     fun puzzle1(): Int {
-        val queue = ArrayDeque<Step>().apply { add(Step(start, start + UP, 0)) }
+        val queue = ArrayDeque<Edge>().apply { add(Edge(start + UP, start, 0)) }
         var maxDist = -1
 
         while (queue.isNotEmpty()) {
-            val (node, prev, dist) = queue.removeFirst()
+            val (prev, node, dist) = queue.removeFirst()
 
             if (node == finish) {
-                maxDist = maxOf(maxDist, dist)
+                maxDist = max(maxDist, dist)
                 continue
             }
 
@@ -34,20 +35,86 @@ class Day23(private val input: List<String>) {
 
             directions
                 .map { direction -> node + direction }
-                .filter { neighbour -> neighbour != prev && neighbour.valid }
-                .forEach { neighbour -> queue.add(Step(neighbour, node, newDist)) }
+                .filter { neighbour -> neighbour != prev && neighbour.space }
+                .forEach { neighbour -> queue.add(Edge(node, neighbour, newDist)) }
         }
 
         return maxDist
     }
 
-    @Suppress("FunctionOnlyReturningConstant")
     fun puzzle2(): Int {
-        return 0
+        val graph = getJunctionGraph()
+        val queue = ArrayDeque<Pair<Set<Point>, Int>>().apply { add(setOf(start) to 0) }
+        var maxDist = -1
+
+        while (queue.isNotEmpty()) {
+            val (path, dist) = queue.removeLast()
+            val node = path.last()
+
+            if (node == finish) {
+                maxDist = max(maxDist, dist)
+                continue
+            }
+
+            graph
+                .filter { (from, till) -> node == from && till !in path }
+                .forEach { (_, till, d) ->
+                    queue.add(path.toMutableSet().apply { add(till) } to dist + d)
+                }
+        }
+
+        return maxDist
+    }
+
+    private fun getJunctionGraph(): List<Edge> {
+        val junctions = input.flatMapIndexed { y, line ->
+            line.indices.mapNotNull { x ->
+                Point(x, y).takeIf { p ->
+                    p.orthogonalNeighbours().count { it.valid && it.value in "><^v" } > 2
+                }
+            }
+        }
+
+        val nodes = listOf(start) + junctions + listOf(finish)
+        val edges = mutableListOf<Edge>()
+        val heads = mutableListOf(start)
+        val queue = ArrayDeque<Pair<Point, Int>>()
+
+        val visited = mutableSetOf<Point>()
+
+        while (heads.isNotEmpty()) {
+            val head = heads.removeFirst()
+            if (head == finish) break
+
+            queue.add(head to 0)
+            visited.add(head)
+
+            while (queue.isNotEmpty()) {
+                val (tail, dist) = queue.removeFirst()
+
+                if (head != tail && tail in nodes) {
+                    heads.add(tail)
+                    visited.remove(tail)
+                    edges.add(Edge(head, tail, dist))
+                    edges.add(Edge(tail, head, dist))
+                    continue
+                }
+
+                tail.orthogonalNeighbours()
+                    .filter { it.valid && it.space && it !in visited }
+                    .forEach { neighbour ->
+                        visited.add(neighbour)
+                        queue.add(neighbour to dist + 1)
+                    }
+            }
+        }
+
+        return edges
     }
 
     private val Point.value get() = input[y][x]
-    private val Point.valid get() = value != '#' // x in 0..<maxX && y in 0..<maxY
+    private val Point.space get() = value != '#'
+    private val Point.valid get() = x in 0..<maxX && y in 0..<maxY
 
-    private data class Step(val node: Point, val prev: Point, val dist: Int)
+    private data class Edge(val from: Point, val till: Point, val dist: Int)
 }
